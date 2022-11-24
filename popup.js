e5a34b6c38ee3e9f8f5bc1a5701f6dfeb0e959d0
ThatguyfromFortnite1 <118480040+ThatguyfromFortnite1@users.noreply.github.com>
2022-11-23 21:54:37 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -17,7 +17,7 @@ function spawnImage(x, y) {
   lastY = y;
 
   var image = document.createElement('img');
-  image.setAttribute('src', './circle.png');
+  image.setAttribute('src', './fireworks-png-30612 (1).png');
   image.setAttribute('class', 'overlays');
   image.setAttribute('width', 128);
   image.setAttribute('height', 128);
